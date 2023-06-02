9376b32d53ee25a3610bceb4d004aa395b88d778
Nasser Brown <80289528+N4SSER@users.noreply.github.com>
2023-06-02 00:52:37 -0600

    Update cumulative_freq.s

--- cumulative_freq.s
+++ cumulative_freq.s
@@ -1,23 +1,29 @@
-@ r0 holds the address of freqs
+.section .data
 
-MOV r1, #0
-LDR r2, =max_pixel_value
-LDR r3, =cu_freqs
-MOV r4, #0
+max_pixel_value: .word 256
 
+.section .data
+
+.comm cu_freqs, 256, 8
+
+.section .text
+.global cumulative_freq
+
+cumulative_freq:
+    PUSH {lr}
+    
+    MOV r1, #0
+    LDR r2, =max_pixel_value
+    LDR r3, =cu_freqs
+    MOV r4, #0
+    
 loop:
     LDR r5, [r0, r1, LSL #2]
     ADD r4, r4, r5
     STR r4, [r3, r5, LSL #2]
     
     ADD r1, r1, #1
-    CMP r1, #max_pixel_value
+    CMP r1, r2
     BLT loop
     
-BX lr
-
-.section .data
-max_pixel_value: .word 256
-
-.section .bss
-.comm cu_freqs, 1024, 4
+    POP {pc}
